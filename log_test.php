aab1c917b05fe8db40c83ea2bd4ebcc671f208f9
serfatyf <serfaty.franck@gmail.com>
2014-04-24 21:37:18 +0200

    dernier jeudi avant la fin bis
    maintenant log_test marche, donc l'utiliser puisque + secure selon
    stephane

--- log_test.php
+++ log_test.php
@@ -12,7 +12,7 @@ if (isset($_GET['pseudo'])){
 	$connexion_stmt->prepare($sql,$bind); 
 	$result = $connexion_stmt->execute($arr);
 	if (!empty($result)) {		// si il y a une reponse, c'est que celui qui a cliqué est un membre
-   		if ($result[0]['mdp'] == $_POST['mdp']) {		// ici on verifie qu'alors son mdp est le bon
+   		if ($result[0]['mdp'] == $_GET['mdp']) {		// ici on verifie qu'alors son mdp est le bon
    			$_SESSION['pseudo'] = $result[0]['pseudo'];	// on ouvre les sessions utiles
    			$_SESSION['id_membre'] = $result[0]['id_membre'];
  			$_SESSION['sexe'] = $result[0]['sexe'];
@@ -26,12 +26,12 @@ if (isset($_GET['pseudo'])){
 	else { 	
 
  		$sql = "SELECT login, id_orga, mdp FROM organisation WHERE `login`=?";
-  		$arr = array($_POST['pseudo']);
+  		$arr = array($_GET['pseudo']);
   		$bind ="s";
   		$connexion_stmt->prepare($sql,$bind); 
   		$result2 = $connexion_stmt->execute($arr);		
   		if (!empty($result2)) {		// si il y a une reponse, c'est que celui qui a cliqué est un membre
-    		if ($result2[0]['mdp']==$_POST['mdp']) {	// si le mdp correspond au mdp entré
+    		if ($result2[0]['mdp']==$_GET['mdp']) {	// si le mdp correspond au mdp entré
     			$_SESSION['pseudo'] = $result2[0]['login'];	// on ouvre les sessions utiles
     			$_SESSION['id_membre'] = $result2[0]['id_orga'];
     			echo "succes";
